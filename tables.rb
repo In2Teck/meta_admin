f911cc33eee6b4e42a6725f6203f9510ffe513b9
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-06-28 17:19:46 -0500

    Added support for relationships and table metadata in data structures. TODO: scaffolding command executin

--- tables.rb
+++ tables.rb
@@ -1,8 +1,33 @@
+TABLES = []
+COLUMNS = {}
+RELATIONSHIPS = {}
+
 def table name
-  puts "table #{name}"
+  TABLES << name
+  COLUMNS[TABLES.last] = {}
+  RELATIONSHIPS[TABLES.last] = {}
 end
 
 def column name, type
-  puts "column #{name} of type #{type}"
+  COLUMNS[TABLES.last][name] = type
 end
 
+def has_many table_array
+  RELATIONSHIPS[TABLES.last]['has_many'] = []
+  table_array.each do |table| 
+    RELATIONSHIPS[TABLES.last]['has_many'] << table
+  end
+end
+
+def belongs_to table_array
+  RELATIONSHIPS[TABLES.last]['belongs_to'] = []
+  table_array.each do |table| 
+    RELATIONSHIPS[TABLES.last]['belongs_to'] << table
+  end
+end
+
+def scaffold
+  puts TABLES
+  puts COLUMNS
+  puts RELATIONSHIPS
+end
--- wikiapodos.dsl
+++ wikiapodos.dsl
@@ -7,3 +7,12 @@ column 'nombre', 'string'
 column 'apellido', 'string'
 column 'email', 'string'
 column 'es_fan', 'boolean'
+has_many ['compartidos']
+
+table 'compartidos'
+column 'usuario_id', 'integer'
+column 'apodo_id', 'integer'
+column 'origen', 'string'
+belongs_to ['usuarios', 'apodos'] 
+
+scaffold
